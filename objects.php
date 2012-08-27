445c256862b53d49d0d8735552b597920e830b3a
Samuel Miller <millers6@null-001b63a9033b.lib.byu.edu>
2012-08-27 11:43:09 -0600

    Added several functions and the functionality for the CAS service check should be working.

--- objects.php
+++ objects.php
@@ -15,13 +15,14 @@
 
 
   // The full info  
-  $arr1 = Array("title"=>"CAS", "status"=>"success", "url"=>"http://cas.byu.edu");
-  $arr2 = Array("title"=>"Person", "status"=>"fail", "url"=>"http://person.byu.edu");
-  $arr3 = Array("title"=>"AIM", "status"=>"success", "url"=>"http://aim.byu.edu");
-  $arr4 = Array("title"=>"GRO", "status"=>"success", "url"=>"http://gro.byu.edu");
-  $arr5 = Array("title"=>"Scout", "status"=>"success", "url"=>"http://scout.byu.edu");
+  $arr1 = Array("title"=>"CAS", "status"=>1, "url"=>"http://cas.byu.edu");
+  $arr2 = Array("title"=>"Person", "status"=>0, "url"=>"http://person.byu.edu");
+  $arr3 = Array("title"=>"AIM", "status"=>1, "url"=>"http://aim.byu.edu");
+  $arr4 = Array("title"=>"GRO", "status"=>1, "url"=>"http://gro.byu.edu");
+  $arr5 = Array("title"=>"Scout", "status"=>1, "url"=>"http://scout.byu.edu");
+  $arr6 = Array("title"=>"Server", "status"=>1, "url"=>"http://learningsuite.byu.edu");
 
-  $all = Array($arr1, $arr2, $arr3, $arr4, $arr5);
+  $all = Array($arr1, $arr2, $arr3, $arr4, $arr5, $arr6);
 
 
   // Just the titles  
@@ -30,8 +31,9 @@
   $title3 = Array("title"=>"AIM");
   $title4 = Array("title"=>"GRO");
   $title5 = Array("title"=>"Scout");
+  $title6 = Array("title"=>"Server");
   
-  $titles = Array($title1, $title2, $title3, $title4, $title5);
+  $titles = Array($title1, $title2, $title3, $title4, $title5, $title6);
 
     
   // Get the requested data
@@ -42,32 +44,157 @@
       break;
       
     case 'cas':
-      $arr = $arr1;
+      $arr = checkCAS();
       break;
 
     case 'person':
-      $arr = $arr2;
+      $arr = checkPersonService();
       break;
 
     case 'aim':
-      $arr = $arr3;
+      $arr = checkAIM();
       break;
 
     case 'gro':
-      $arr = $arr4;
+      $arr = checkGRO();
       break;
 
     case 'scout':
-      $arr = $arr5;
+      $arr = checkScout();
       break;
     
     case 'all':
     default:
-      $arr = $all;
+      $arr = checkAllServices();
       break;
   }  
-  
-  
+
+/**
+* Will check all services and 
+*
+* @return A json encoded array
+*/
+function checkAllServices(){
+  $retArr = [];
+
+  //TODO: Remove 'magic' strings
+  $retArr[] = json_decode(checkServer());
+  $retArr[] = json_decode(checkCAS());
+  $retArr[] = json_decode(checkPersonService());
+  $retArr[] = json_decode(checkGRO());
+
+  return json_encode($retArr);
+}
+
+/**
+* Will check too see if the service that hosts the website works
+*
+* @return A json encoded array
+*/
+function checkServer(){
+
+  $retArr = [];
+  $retArr["title"] = "Server";
+  $retArr["status"] = 1;
+  $retArr["url"] = "my.byu.edu";
+
+  return json_encode($retArr);
+}
+
+/**
+* Will check to see if the CAS system is up an running
+*
+* @return A json encoded array
+*/
+function checkCAS(){
+  //TODO: Make into a defined
+  $CASurl = "https://cas.byu.edu/cas/login?service=https://my.byu.edu/uPortal/Login";
+  if($CASurl == NULL) return false;
+
+  //Connect to website with curl
+  $curly = curl_init($CASurl);
+
+  //Some settings for curl
+  curl_setopt($curly, CURLOPT_TIMEOUT, 5);
+  curl_setopt($curly, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($curly, CURLOPT_RETURNTRANSFER, true);// What does this exactly do
+
+  $data = curl_exec($curly);
+  $httpcode = curl_getinfo($curly,CURLINFO_HTTP_CODE);
+
+  //Close curl
+  curl_close($curly);
+
+  //Return object
+  $retArr = [];
+  $retArr["title"] = "CAS";
+  $retArr["url"] = "cas.byu.edu";
+
+  if($httpcode >= 200 && $httpcode < 300){
+    $retArr["status"] = 1;
+  }else{
+    $retArr["status"] = 0;
+  }
+
+  return json_encode($retArr);
+}
+
+/**
+* Will check the person service and see if it is working or not
+*
+* @return A json encoded array
+*/
+function checkPersonService(){
+  $retArr = [];
+  $retArr["title"] = "Person";
+  $retArr["status"] = 0;
+  $retArr["url"] = "person.byu.edu";
+
+  return json_encode($retArr);
+
+}
+
+/**
+* Will check the gro service and giveback a status
+* 
+* @return A json encoded array
+*/
+function checkGRO(){
+  $retArr = [];
+  $retArr["title"] = "GRO";
+  $retArr["status"] = 0;
+  $retArr["url"] = "gro.byu.edu";
+
+  return json_encode($retArr);
+}
+
+/**
+* Will check the AIM service
+*
+* @return A json encoded array
+*/
+function checkAIM(){
+  $retArr = [];
+  $retArr["title"] = "AIM";
+  $retArr["status"] = 0;
+  $retArr["url"] = "aim.byu.edu";
+
+  return json_encode($retArr);
+}
+
+/**
+* Will check the Scout service
+*
+* @return A json encoded array
+*/
+function checkScout(){
+  $retArr = [];
+  $retArr["title"] = "Scout";
+  $retArr["status"] = 0;
+  $retArr["url"] = "scout.byu.edu";
+
+  return json_encode($retArr);
+}
   // Send back the JSON
   echo json_encode($arr);
   
